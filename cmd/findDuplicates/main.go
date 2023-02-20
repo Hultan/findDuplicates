4ef3e61b637b890aaafe54af91603452ee68948f
per <per@softteam.se>
2023-02-20 12:57:56 +0100

    version 2.0 - No database

--- cmd/findDuplicates/main.go
+++ cmd/findDuplicates/main.go
@@ -3,221 +3,97 @@ package main
 import (
 	"bufio"
 	"crypto/md5"
-	"database/sql"
-	"errors"
 	"fmt"
-	"io"
-	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
-	"regexp"
-	"time"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
-const (
-	testDbPath = "/home/per/code/findDuplicates/test/data/test.db"
-)
+const hashSize = 65536
 
-type File struct {
-	Path string
-	Hash string
+type hashedFile struct {
+	hash string
+	path string
+	err  error
 }
 
-const dbPath = "/home/per/files.db"
-
-var scanPath = ""
+type fileList []string
+type results map[string]fileList
 
 func main() {
-	code, ok := validateArgs()
-	if !ok {
-		os.Exit(code)
+	if len(os.Args) < 2 {
+		log.Fatal("Missing parameter, provide dir name!")
 	}
 
-	// Create a files.db database
-	createDatabaseStep()
+	fmt.Println("Scanning files...")
+	fmt.Println()
 
-	// Scan directory and create hashes for the files
-	calculateHashStep()
-
-	// List all duplicate hashes
-	listDuplicatesStep()
-
-	// Delete database, we start with a new database every time
-	deleteDatabaseStep()
-}
-
-func validateArgs() (int, bool) {
-	if len(os.Args) != 2 {
-		fmt.Println("USAGE : findDuplicates [ScanPath]")
-		fmt.Println("See Obsidian for how to mount drives...")
-		return 0, false
-	}
-
-	scanPath = os.Args[1]
-	if !directoryExists(scanPath) {
-		msg := "scan path does not exist, have you forgotten to mount the drive? See Obsidian...\n"
-		_, _ = fmt.Fprintf(os.Stderr, msg)
-		return 1, false
-	}
-
-	return 0, true
-}
-
-func createDatabaseStep() {
-	fmt.Printf("Creating database at : %s\n\n", dbPath)
-
-	db, err := createDatabase(dbPath)
+	hashes, err := searchTree(os.Args[1])
 	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "Failed to create database! Reason = %s\n", err)
-		os.Exit(1)
+		msg := fmt.Sprintf("Failed to search tree : %s", err)
+		log.Fatal(msg)
 	}
 
-	_ = db.Close()
-}
-
-func calculateHashStep() {
-	db := openDatabase(dbPath)
-	defer func(db *sql.DB) {
-		_ = db.Close()
-	}(db)
-
-	start := time.Now()
-	fmt.Println("Scanning directory...")
-	fileNames, err := scanDirectory(scanPath, "(.avi|.AVI|.mkv|.mp4|.MP4|.mpg|.wmv)$")
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "scanDirectory() failed! Reason = %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("Scan result : found %d files (%s).\n\n", len(fileNames), time.Since(start).String())
-
-	start = time.Now()
-	fmt.Println("Generating hashes...")
-	files, count := generatingHashes(fileNames)
-	fmt.Printf("Generate result : generated hashes for %d files (%s).\n\n", count, time.Since(start).String())
-
-	start = time.Now()
-	fmt.Println("Inserting hashes into db...")
-	count = 0
-	for _, file := range files {
-		// fmt.Printf("generating hash for %s...\n", file.Path)
-		err = insertFileWithHash(db, file.Path, file.Hash)
-		if err != nil {
-			// Failed to insert file, log and continue
-			_, _ = fmt.Fprintf(os.Stderr, "failed to insert file inte database! Reason = %s\n", err)
+	for hash, files := range hashes {
+		// Ignore non duplicates
+		if len(files) == 1 {
 			continue
 		}
-		count++
-	}
-	fmt.Printf("Insert result : inserted hashes for %d files (%s).\n", count, time.Since(start).String())
-}
 
-func listDuplicatesStep() {
-	db := openDatabase(dbPath)
-	defer func(db *sql.DB) {
-		_ = db.Close()
-	}(db)
-
-	dup, err := findDuplicates(db)
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "failed to find duplicates! Reason = %s", err)
-		os.Exit(1)
-	}
-
-	fmt.Println()
-	fmt.Println("============")
-	fmt.Println(" Duplicates ")
-	fmt.Println("============")
-	fmt.Println()
-	fmt.Printf("%d duplicates found...\n", len(dup))
-	fmt.Println()
-	for _, file := range dup {
-		fmt.Printf("%s\t%s\n", file.Hash, file.Path)
-	}
-}
-
-func deleteDatabaseStep() {
-	err := os.Remove(dbPath)
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "failed to remove database at %s...", dbPath)
-		os.Exit(1)
-	}
-}
-
-func scanDirectory(dir, pattern string) ([]string, error) {
-	// Needs to be declared like this (and not nil slice) for test to work
-	result := []string{}
-
-	err := filepath.WalkDir(
-		dir, func(s string, d fs.DirEntry, err error) error {
-			if err != nil {
-				return err
-			}
-			if !d.IsDir() {
-				ok, err := regexp.Match(pattern, []byte(s))
-				if err != nil {
-					return err
-				}
-				if ok {
-					result = append(result, s)
-				}
-			}
-			return nil
-		},
-	)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
-
-func generatingHashes(fileNames []string) ([]*File, int) {
-	var files []*File
-	gen := 0
-
-	// For each file, calculate hash and generate a File{} struct
-	for _, fileName := range fileNames {
-		file := &File{Path: fileName}
-
-		hash, err := calculateHash(file.Path)
-		if err != nil {
-			panic(err)
+		// Report duplicate, we will use just 7 chars like git
+		fmt.Printf("%s - %d files", hash[len(hash)-7:], len(files))
+		for _, file := range files {
+			fmt.Println("  ", file)
 		}
-		file.Hash = hash
-
-		files = append(files, file)
-		gen++
+		fmt.Println()
 	}
-	return files, gen
 }
 
-func calculateHash(filePath string) (string, error) {
-	f, err := os.Open(filePath)
+func hashFile(path string) hashedFile {
+	f, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer func() {
-		if err = f.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
+	defer f.Close()
 
-	// Read the first 64k of the file
+	// Read the first section of the file
+	b := make([]byte, hashSize)
 	r := bufio.NewReader(f)
-	b := make([]byte, 65536)
 	n, err := r.Read(b)
-	if err == io.EOF {
-		return "", errors.New("file is empty")
-	}
-	if err != nil {
-		return "", err
-	}
 
 	// Return MD5 hash of the file
 	m := md5.New()
-	return fmt.Sprintf("%x", m.Sum(b[0:n])), nil
+	return hashedFile{
+		hash: fmt.Sprintf("%x", m.Sum(b[0:n])),
+		path: path,
+		err:  err,
+	}
+}
+
+func searchTree(dir string) (results, error) {
+	hashes := make(results)
+
+	visit := func(p string, fi os.FileInfo, err error) error {
+		if err != nil && err != os.ErrNotExist {
+			return err
+		}
+
+		if fi.Mode().IsRegular() && fi.Size() > 0 {
+			h := hashFile(p)
+
+			// Report failed hashes, and ignore them
+			if h.err != nil {
+				fmt.Printf("Failed to hash file %s : %s", h.path, h.err)
+				return nil
+			}
+
+			hashes[h.hash] = append(hashes[h.hash], h.path)
+		}
+
+		return nil
+	}
+
+	err := filepath.Walk(dir, visit)
+
+	return hashes, err
 }
